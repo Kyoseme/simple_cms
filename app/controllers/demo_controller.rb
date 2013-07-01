6c841178fc4ab293c86bfab1f88d0f04eb78bf50
Pasi Lindholm <pasilindholm@Pasis-MacBook-Air.local>
2013-07-01 22:24:36 +0300

    hgkkjhjkjkhg

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,10 +1,18 @@
 class DemoController < ApplicationController
 
   def index
-      render(:action => 'hello')
+      #render('hello')
+     # redirect_to(:action => 'other_hello')
   end
 
   def hello
+    @array = [1,2,3,4,5]
+    @id = params[:id]
+    @page = params[:page]
+  end
+
+  def other_hello
+      render(:text => 'Hello everyone!')  
   end
 
 end
---
+++ db/migrate/20130701183616_do_nothing_yet.rb
@@ -0,0 +1,10 @@
+class DoNothingYet < ActiveRecord::Migration
+  def up
+    #instructions for changing the db to a new state
+  end
+
+  def down
+    #instructions for changing the db to a new state
+    
+  end
+end
