ec44874d392b38f247092cb57eaada76b79dd3e1
Pasi Lindholm <pasilindholm@Pasis-MacBook-Air.local>
2013-07-05 16:54:00 +0300

    asdasda

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ActiveRecord::Base
+
    attr_acc essible :title, :body, :subject_id, :name, :permalink, :position, :visible
+  
+   belongs_to :subject
   
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,8 @@ class Subject < ActiveRecord::Base
    
    attr_accessible :title, :body, :name, :position, :visible
    
+   has_one :page
+   
    scope :visible, where(:visible => true)
    scope :invisible, where(:visible => false)
    scope :search, lambda {|query| where(["name LIKE ?", "%#[{query}"])}
